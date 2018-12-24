d3b494f43d5a0b817d3c06766db40df8eaf608e2
jack <jack@codeloom.co.uk>
2018-12-24 15:08:15 +0000

    add arrKey prop back

--- src/core/plugins/oas3/wrap-components/parameters.jsx
+++ src/core/plugins/oas3/wrap-components/parameters.jsx
@@ -300,6 +300,7 @@ class Parameters extends Component {
             specActions={specActions}
             pathMethod={pathMethod}
             isExecute={isExecute}
+            arrKey={arrKey}
           />
         </tr>
       );
